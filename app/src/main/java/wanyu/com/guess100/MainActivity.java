b4c2410be3c163e763e46947d073da013471d238
wanyu0409 <jasperwendy0409@gmail.com>
2017-11-14 10:48:53 +0800

    Guess

--- app/src/main/java/wanyu/com/guess100/MainActivity.java
+++ app/src/main/java/wanyu/com/guess100/MainActivity.java
@@ -1,12 +1,20 @@
 package wanyu.com.guess100;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    Random ran = new Random();
+    int r = ran.nextInt(99)+1;
+    int min = 1;
+    int max = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,22 @@ public class MainActivity extends AppCompatActivity {
         TextView secret = (TextView) findViewById(R.id.secret);
         TextView info = (TextView) findViewById(R.id.info);
         Button send = (Button) findViewById(R.id.send);
+        int n = Integer.parseInt(number.getText().toString());
 
+
+        while (n>r && n>min && n<max){
+            Toast.makeText(MainActivity.this, min+"to"+r, Toast.LENGTH_LONG).show();
+            TextView number100 = (TextView) findViewById(R.id.number);
+            String s = String.valueOf(n);
+            number100.setText(s);
+            break;
+        }
+        while (n<r && n>min && n<max){
+            Toast.makeText(MainActivity.this, n+"to"+r , Toast.LENGTH_SHORT).show();
+            TextView number0 = (TextView) findViewById(R.id.number);
+            String s = String.valueOf(n);
+            number0.setText(s);
+            break;
+        }
     }
 }
