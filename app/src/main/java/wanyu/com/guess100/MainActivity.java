38496a5a745d4ae3c325c1f1744a34338ccb49ae
wanyu0409 <jasperwendy0409@gmail.com>
2017-11-14 10:09:51 +0800

    findViews

--- app/src/main/java/wanyu/com/guess100/MainActivity.java
+++ app/src/main/java/wanyu/com/guess100/MainActivity.java
@@ -2,6 +2,9 @@ package wanyu.com.guess100;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+    }
+    public void findViews(){
+        EditText number = (EditText) findViewById(R.id.number);
+        TextView secret = (TextView) findViewById(R.id.secret);
+        TextView info = (TextView) findViewById(R.id.info);
+        Button send = (Button) findViewById(R.id.send);
+
     }
 }
